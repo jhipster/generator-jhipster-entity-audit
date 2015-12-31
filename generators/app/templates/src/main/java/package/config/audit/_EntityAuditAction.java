0bc595c2d0e5b077ed35201d6477dc2d6c4d7f72
Deepu k Sasidharan <d4udts@gmail.com>
2015-12-31 14:29:32 +0800

    add templates

---
+++ generators/app/templates/src/main/java/package/config/audit/_EntityAuditAction.java
@@ -0,0 +1,25 @@
+package <%=packageName%>.config.audit;
+
+/**
+ * Enum for the different audit actions
+ */
+public enum EntityAuditAction {
+    CREATE("CREATE"),
+    UPDATE("UPDATE"),
+    DELETE("DELETE");
+
+    private String value;
+
+    EntityAuditAction(final String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value();
+    }
+}
---
+++ generators/app/templates/src/main/webapp/scripts/app/admin/entityAudit/_entityAudit.detail.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('<%=angularAppName%>')
+    .controller('AuditDetailModalCtrl', function ($scope, $uibModalInstance, $sce, ObjectDiff, diff, audit) {
+
+        $scope.diffValue = $sce.trustAsHtml(ObjectDiff.toJsonView(diff));
+        $scope.diffValueChanges = $sce.trustAsHtml(ObjectDiff.toJsonDiffView(diff));
+        $scope.audit = audit;
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
