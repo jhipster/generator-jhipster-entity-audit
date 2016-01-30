506e3734090b4ab665db662f31064571768265eb
Frederik Hahne <frederik.hahne@gmail.com>
2016-01-30 11:37:00 +0100

    update list of audited entities when generating a new entity

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -47,6 +47,24 @@ module.exports = yeoman.generators.Base.extend({
       this.log(chalk.white('Running ' + chalk.bold('JHipster Entity Audit') + ' Generator! ' + chalk.yellow('v' + packagejs.version + '\n')));
     },
 
+    getEntitityNames: function () {
+      var existingEntities = [],
+      existingEntityNames = [];
+      try{
+        existingEntityNames = fs.readdirSync('.jhipster');
+      } catch(e) {
+        this.log(chalk.red.bold('ERROR!') + ' Could not read entities, you might not have generated any entities yet. I will continue to install audit files, entities will not be updated...\n');
+      }
+
+      existingEntityNames.forEach(function(entry) {
+        if(entry.indexOf('.json') !== -1){
+          var entityName = entry.replace('.json','');
+          existingEntities.push(entityName);
+        }
+      });
+      this.existingEntities = existingEntities;
+    },
+
     validate: function () {
       // this shouldnt be run directly
       if (!this.entityConfig) {
@@ -59,11 +77,12 @@ module.exports = yeoman.generators.Base.extend({
     if (this.abort){
       return;
     }
+
     // don't prompt if data are imported from a file
     if (this.entityConfig.useConfigurationFile == true &&  this.entityConfig.data && typeof this.entityConfig.data.enableEntityAudit !== 'undefined') {
       this.enableAudit = this.entityConfig.data.enableEntityAudit;
 
-      if (typeof this.config.get('auditFramework'); !== 'undefined') {
+      if (typeof this.config.get('auditFramework') !== 'undefined') {
         this.auditFramework = this.config.get('auditFramework');
       } else {
         this.auditFramework = 'custom';
@@ -139,9 +158,22 @@ module.exports = yeoman.generators.Base.extend({
           // add javers annotations to repository
           jhipsterFunc.replaceContent(this.javaDir + 'repository/' + entityName + 'Repository.java', 'public interface ' + entityName + 'Repository', '@JaversSpringDataAuditable\npublic interface ' + entityName + 'Repository');
           jhipsterFunc.replaceContent(this.javaDir + 'repository/' + entityName + 'Repository.java', 'domain.' + entityName + ';', 'domain.' + entityName + ';\nimport org.javers.spring.annotation.JaversSpringDataAuditable;');
+
+          //update the list of audited entities if audit page is available
+          if (this.fs.exists(this.javaDir + 'web/rest/EntityAuditResource.java')) {
+            this.existingEntities.push(entityName);
+            this.auditedEntities = [];
+
+            this.existingEntities.forEach(function(entityName) {
+                this.auditedEntities.push("\"" + entityName + "\"")
+            }
+
+            var files = [
+              { from: this.javaTemplateDir + '/web/rest/_JaversEntityAuditResource.java', to: this.javaDir + 'web/rest/JaversEntityAuditResource.java'}
+            ];
+            this.copyFiles(files);
+          }
         }
-
-
       }
     },
     updateConfig : function() {
