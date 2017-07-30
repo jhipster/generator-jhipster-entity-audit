02ec32c19b44658e7424ca5dca16fcc0cf0a8f0b
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-07-30 22:00:33 +0200

    Travis build

---
+++ travis/scripts/02-generate-project.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+#-------------------------------------------------------------------------------
+# Force no insight
+#-------------------------------------------------------------------------------
+mkdir -p "$HOME"/.config/configstore/
+mv "$JHIPSTER_TRAVIS"/configstore/*.json "$HOME"/.config/configstore/
+
+#-------------------------------------------------------------------------------
+# Generate the project with yo jhipster
+#-------------------------------------------------------------------------------
+if [[ "$JHIPSTER" == *"uaa"* ]]; then
+    mkdir -p "$UAA_APP_FOLDER"
+    mv -f "$JHIPSTER_SAMPLES"/uaa/.yo-rc.json "$UAA_APP_FOLDER"/
+    cd "$UAA_APP_FOLDER"
+    yarn link generator-jhipster
+    jhipster --force --no-insight --with-entities --skip-checks
+    ls -al "$UAA_APP_FOLDER"
+fi
+
+mkdir -p "$APP_FOLDER"
+mv -f "$JHIPSTER_SAMPLES"/"$JHIPSTER"/.yo-rc.json "$APP_FOLDER"/
+cd "$APP_FOLDER"
+yarn link generator-jhipster
+jhipster --force --no-insight --skip-checks --with-entities
+ls -al "$APP_FOLDER"
--- travis/scripts/03-generate-audit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ev
-#-------------------------------------------------------------------------------
-# Generate the default audit behaviour
-#-------------------------------------------------------------------------------
-cd "$HOME"/app
-yo jhipster-entity-audit default --force --no-insight
