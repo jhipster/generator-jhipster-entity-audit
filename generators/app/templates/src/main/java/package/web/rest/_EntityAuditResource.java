ef15f96a1519db4b7501ea9e4aafe80bfe18e727
Deividi <deividi.cavarzan@gmail.com>
2019-02-26 12:41:00 -0300

    Update _EntityAuditResource.java

--- generators/app/templates/src/main/java/package/web/rest/_EntityAuditResource.java
+++ generators/app/templates/src/main/java/package/web/rest/_EntityAuditResource.java
@@ -18,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.access.annotation.Secured;
-import com.codahale.metrics.annotation.Timed;
 
 import java.net.URISyntaxException;
 import java.util.List;
 import io.micrometer.core.annotation.Timed;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * REST controller for getting the audit events for entity
