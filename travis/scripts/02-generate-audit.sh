ab1ac8912f4841951092d40823bca5e8f2af08bf
Aurélien Mino <aurelien.mino@gmail.com>
2020-01-16 23:10:25 +0100

    Removed former integration tests

--- travis/scripts/02-generate-audit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-#-------------------------------------------------------------------------------
-# Generate the default audit behaviour
-#-------------------------------------------------------------------------------
-cd "$APP_FOLDER"
-npm link generator-jhipster-entity-audit
-yo jhipster-entity-audit default --force --no-insight
