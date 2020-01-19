5c38f694f20df786c3657c47642f6a7cd75b02fb
Aurélien Mino <aurelien.mino@gmail.com>
2020-01-19 15:43:40 +0100

    Unused import

--- generators/app/templates/src/main/java/package/web/rest/_EntityAuditResource.java
+++ generators/app/templates/src/main/java/package/web/rest/_EntityAuditResource.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
