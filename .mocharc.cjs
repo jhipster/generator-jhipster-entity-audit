7200765fef011629e8b9506e3daadc0c80b0a64c
Marcelo Shima <marceloshima@gmail.com>
2023-08-21 20:27:19 -0300

    convert tests to esmocha (#206)

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -6,6 +6,5 @@ module.exports = {
   ui: 'bdd',
   extension: ['spec.js', 'spec.cjs', 'spec.mjs'],
   exclude: ['**/resources/**', '**/node_modules/**'],
-  require: 'mocha-expect-snapshot',
   parallel: true,
 };
