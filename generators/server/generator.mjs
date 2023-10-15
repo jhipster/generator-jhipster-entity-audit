480f0a6a78e57b4634068ee67bdfd6654f859e38
Marcelo Shima <marceloshima@gmail.com>
2023-10-15 12:37:29 -0300

    prompt adjusts (#213)
    * prompt adjusts
    
    * add AbstractAuditingEntity test

--- generators/server/generator.mjs
+++ generators/server/generator.mjs
@@ -2,12 +2,17 @@ import BaseApplicationGenerator from 'generator-jhipster/generators/base-applica
 import command from './command.mjs';
 
 export default class extends BaseApplicationGenerator {
+  initialRun;
+
   constructor(args, opts, features) {
     super(args, opts, { ...features, sbsBlueprint: true });
   }
 
   get [BaseApplicationGenerator.INITIALIZING]() {
     return this.asInitializingTaskGroup({
+      setInitialRun() {
+        this.initialRun = this.blueprintConfig.auditFramework === undefined;
+      },
       async initializeOptions() {
         this.parseJHipsterCommand(command);
       },
@@ -16,9 +21,7 @@ export default class extends BaseApplicationGenerator {
 
   get [BaseApplicationGenerator.PROMPTING]() {
     return this.asPromptingTaskGroup({
-      async promptingTemplateTask({ control }) {
-        if (control.existingProject && !this.options.askAnswered) return;
-
+      async promptingTemplateTask() {
         await this.prompt(this.prepareQuestions(command.configs));
       },
     });
@@ -28,7 +31,7 @@ export default class extends BaseApplicationGenerator {
     return this.asComposingTaskGroup({
       async composingTask() {
         if (this.blueprintConfig.auditFramework && this.blueprintConfig.auditFramework !== 'no') {
-          const auditedEntities = this.updateType === 'all' ? this.getExistingEntities().map(e => e.name) : this.auditedEntities;
+          const auditedEntities = this.auditUpdateType === 'all' ? this.getExistingEntities().map(e => e.name) : this.auditedEntities;
           await this.composeWithJHipster('jhipster-entity-audit:java-audit', { generatorOptions: { auditedEntities } });
         }
       },
