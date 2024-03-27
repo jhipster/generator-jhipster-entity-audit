f830d575d467fe753988fc90dfeded2c6303ae75
Marcelo Shima <marceloshima@gmail.com>
2024-03-26 21:39:22 -0300

    use mock at angular-audit generator

--- generators/angular-audit/generator.spec.js
+++ generators/angular-audit/generator.spec.js
@@ -14,6 +14,7 @@ describe('SubGenerator angular-audit of entity-audit JHipster blueprint', () => 
         .withOptions({
           ignoreNeedlesError: true,
         })
+        .withMockedSource()
         .withJHipsterLookup()
         .withParentBlueprintLookup();
     });
