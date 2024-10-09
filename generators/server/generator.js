ff406dc04adc991f8b53bb0ce41443b731e49f31
Marcelo Shima <marceloshima@gmail.com>
2024-10-09 14:33:46 -0300

    Update generator.js

--- generators/server/generator.js
+++ generators/server/generator.js
@@ -1,28 +1,14 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
 
 export default class extends BaseApplicationGenerator {
-  initialRun;
-  auditedEntities;
-
   constructor(args, opts, features) {
     super(args, opts, { ...features, queueCommandTasks: true, sbsBlueprint: true });
-  }
-
-  get [BaseApplicationGenerator.INITIALIZING]() {
-    return this.asInitializingTaskGroup({
-      setInitialRun() {
-        this.initialRun = this.blueprintConfig.auditFramework === undefined;
-      },
-    });
   }
 
   get [BaseApplicationGenerator.COMPOSING]() {
     return this.asComposingTaskGroup({
       async composingTask() {
-        if (this.blueprintConfig.auditFramework && this.blueprintConfig.auditFramework !== 'no') {
-          const auditedEntities = this.auditUpdateType === 'all' ? this.getExistingEntities().map(e => e.name) : this.auditedEntities;
-          await this.composeWithJHipster('jhipster-entity-audit:java-audit', { generatorOptions: { auditedEntities } });
-        }
+        await this.composeWithJHipster('jhipster-entity-audit:java-audit');
       },
     });
   }
