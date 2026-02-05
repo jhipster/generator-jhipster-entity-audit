eb46679f794788678be2343cbaf8b49e73bfcdec
Marcelo Shima <marceloshima@gmail.com>
2026-02-04 21:29:06 -0300

    cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,8 @@ import { defaultExclude, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     pool: 'forks',
-    environment: 'node',
     hookTimeout: 20000,
     exclude: [...defaultExclude.filter(val => val !== '**/cypress/**'), '**/templates/**', '**/resources/**'],
     setupFiles: ['./vitest.test-setup.ts'],
-    clearMocks: true,
   },
 });
